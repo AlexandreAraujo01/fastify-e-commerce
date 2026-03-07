526da3f99a4d216e9dffb7a248ae85f93da87316
Alex <alexandre.roberto115@gmail.com>
2026-03-07 11:09:08 -0300

    actions: adding main branch protection

--- vitest.config.base.ts
+++ vitest.config.base.ts
@@ -2,16 +2,16 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    // Substitui o defineWorkspace e unifica os projetos
-    projects: ["./apps/*", "./packages/*"],
-    coverage: {
-      provider: "v8",
-      enabled: true,
-      // O json-summary é o que a Action do GitHub precisa para comentar no PR
-      reporter: ["text", "json", "json-summary"],
-      // Centraliza todos os relatórios na pasta coverage da raiz
-      reportsDirectory: "./coverage",
-    },
-  },
+	test: {
+		// Substitui o defineWorkspace e unifica os projetos
+		projects: ["./apps/*", "./packages/*"],
+		coverage: {
+			provider: "v8",
+			enabled: true,
+			// O json-summary é o que a Action do GitHub precisa para comentar no PR
+			reporter: ["text", "json", "json-summary"],
+			// Centraliza todos os relatórios na pasta coverage da raiz
+			reportsDirectory: "./coverage",
+		},
+	},
 });
