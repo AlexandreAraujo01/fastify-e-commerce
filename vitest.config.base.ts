7fa2571d68f60992296d9a202b3e09b3a06c4e4c
Alex <alexandre.roberto115@gmail.com>
2026-03-07 13:50:43 -0300

    fix: fixing fakeHash import and solving yml error

--- vitest.config.base.ts
+++ vitest.config.base.ts
@@ -1,17 +1,17 @@
 // vitest.config.ts (na raiz do projeto)
 import { defineConfig } from "vitest/config";
-
+import path from "node:path";
 export default defineConfig({
-	test: {
-		// Substitui o defineWorkspace e unifica os projetos
-		projects: ["./apps/*", "./packages/*"],
-		coverage: {
-			provider: "v8",
-			enabled: true,
-			// O json-summary é o que a Action do GitHub precisa para comentar no PR
-			reporter: ["text", "json", "json-summary"],
-			// Centraliza todos os relatórios na pasta coverage da raiz
-			reportsDirectory: "./coverage",
-		},
-	},
+  test: {
+    // Substitui o defineWorkspace e unifica os projetos
+    projects: ["./apps/*", "./packages/*"],
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      // O json-summary é o que a Action do GitHub precisa para comentar no PR
+      reporter: ["text", "json", "json-summary"],
+      // Centraliza todos os relatórios na pasta coverage da raiz
+      reportsDirectory: "./coverage",
+    },
+  },
 });
